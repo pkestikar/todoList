9fbdf90d25691b80c622325be28f2e19be315b59
Prathmesh Kestikar <kestikar.p@gameonmedia.ie>
2019-07-16 12:01:01 +0100

    updates

---
+++ app/index.js
@@ -0,0 +1,4 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>Hello render this on the page</div>, document.getElementById('root'));
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: "./app/index.js",
     output: {
         filename: "main.js",
