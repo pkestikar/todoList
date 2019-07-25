9077864d8cb4b46825d4f7ac734acadeefbdc737
Prathmesh Kestikar <kestikar.p@gameonmedia.ie>
2019-07-25 11:46:10 +0100

    added the mesage on the left side

--- app/components/TodoPad/index.js
+++ app/components/TodoPad/index.js
@@ -4,7 +4,24 @@ import TopBar from 'Components/TopBar';
 const TodoPad = (props) => {
     return (
         <div className="todo-pad">
-            <TopBar/>
+            <div className="todo-pad-content">
+                <TopBar/>
+                <div className="todo-wrapper">
+                    <div className="message">
+                        <p>
+                            Hello {'<user>'}, <br/>
+                            welcome back. <br/>
+                            You have {'<count>'} <br/>
+                            remaining tasks <br/>
+                            to complete <br/>
+                            this week. <br/>
+                        </p>
+                    </div>
+                    <div className="the-list">
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
