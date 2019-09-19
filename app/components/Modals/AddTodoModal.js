72bf285215cf52e1c2ae7b9c8e3481036dbe45d4
Prathmesh Kestikar <kestikar.p@gameonmedia.ie>
2019-09-19 17:29:57 +0100

    updates

--- app/components/Modals/AddTodoModal.js
+++ app/components/Modals/AddTodoModal.js
@@ -2,15 +2,31 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Icon from 'Components/Icon';
 import {hideModal} from "../../actions/modal";
+import onClickOutside from 'react-onClickOutside';
 
 const AddTodoModal = props => {
+
+    AddTodoModal.handleClickOutside = evt => {
+        props.hideAddTodoModal();
+    };
+
     return (
         <div className={'modal__add-todo'}>
             <div className="close-button" onClick={() => props.hideAddTodoModal()}>
                 <Icon icon={'close'}/>
             </div>
+
+            <div className="input-wrapper">
+                <input type={'text'} placeholder={'Short task description'}/>
+                <input type={'text'} placeholder={'Task description'}/>
+            </div>
+
         </div>
     )
+};
+
+const clickOutsideConfig = {
+    handleClickOutside: () => AddTodoModal.handleClickOutside
 };
 
 const mapStateToProps = state => {
@@ -25,4 +41,4 @@ const mapDispatchToProps = dispatch => {
     }
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddTodoModal);
+export default connect(mapStateToProps, mapDispatchToProps)(onClickOutside(AddTodoModal, clickOutsideConfig));
